60ad9be98889c4ab7b6eba3365fae28f12f9858d
apedano <pedano.alessandro@gmail.com>
2024-01-12 15:02:30 +0100

    First commit

---
+++ src/test/java/com/apedano/ldapsecuritydemo/LdapSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apedano.ldapsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
